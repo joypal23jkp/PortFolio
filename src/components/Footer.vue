0f8ea655bdf05d05ed9acbd09f60695bc5561110
joypa <joypal.23.jkp@gmail.com>
2022-05-18 21:30:34 +0600

    home page changed.

---
+++ src/components/Footer.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="footer_wrapper flex items-center justify-center">
+    <div
+      class="footer absolute top-0 left-0 h-full w-full pt-32 flex items-center justify-center"
+    >
+      <div class="max-h-6/10 text-center">
+        <h1 class="font-DM font-black text-5xl">Get in touch!</h1>
+        <h5 class="font-DM font-bold text-pastButton text-4xl mt-10">
+          {{ userProfile.email }}
+        </h5>
+        <h5 class="font-DM font-bold text-3xl text-pastButton mt-5">
+          {{ userProfile.phone }}
+        </h5>
+
+        <div>
+          <ul class="flex justify-evenly mt-24 max-w-lg mx-auto">
+            <li v-for="(social, index) in userProfile.socials" :key="index">
+              <router-link :to="social.link">
+                <img
+                  :src="social.img"
+                  :alt="social.name"
+                  width="20"
+                  height="20"
+                />
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footer",
+  props: {
+    profile: {
+      type: Object,
+    },
+  },
+  data: function () {
+    return {
+      userProfile: this.profile,
+    };
+  },
+};
+</script>
+<style>
+.footer_wrapper {
+  background-image: url("../assets/images/Hero-Background.svg");
+  min-height: 350px;
+  background-position: bottom;
+  background-repeat: no-repeat;
+  background-size: cover;
+  transform: rotate(180deg);
+  position: relative !important;
+}
+
+.footer {
+  transform: rotate(180deg);
+}
+</style>
---
+++ src/components/Writes/Article.vue
@@ -0,0 +1,107 @@
+<template>
+  <template v-if="articleType === 'weekly'">
+    <div
+      class="flex w-11/12 mx-auto border rounded shadow items-center justify-center my-20"
+      style="height: 243px"
+    >
+      <div class="w-3/5 p-20">
+        <h1 class="font-bold text-5xl">{{ article.title }}</h1>
+        <p class="text-xl my-5" style="color: #4a5568">{{ article.body }}</p>
+        <div class="flex justify-between">
+          <span class="text-xl" style="color: #4a5568">{{
+            article.created_at
+          }}</span>
+          <span
+            class="font-bold text-xl cursor-pointer opacity-100 hover:opacity-70"
+            >Read more</span
+          >
+        </div>
+      </div>
+      <div class="w-2/5 h-full">
+        <img class="h-full" :src="article.image" :alt="article.title" />
+      </div>
+    </div>
+  </template>
+  <template v-else-if="articleType === 'item'">
+    <div class="border rounded shadow items-center justify-center">
+      <div class="w-full">
+        <img class="h-full" :src="article.image" :alt="article.title" />
+      </div>
+      <div class="w-full p-6 text-left">
+        <h1 class="font-bold text-3xl">{{ article.title }}</h1>
+        <p class="text-lg my-5" style="color: #4a5568">{{ article.body }}</p>
+        <div class="flex justify-between">
+          <span class="text-lg" style="color: #4a5568">{{
+            article.created_at
+          }}</span>
+          <span
+            class="font-bold text-lg cursor-pointer opacity-100 hover:opacity-70"
+            >Read more</span
+          >
+        </div>
+      </div>
+    </div>
+  </template>
+  <template v-else>
+    <div
+      class="flex w-11/12 border rounded shadow items-center justify-center mx-auto my-32"
+      style="min-height: 450px"
+    >
+      <div class="w-1/2 p-32 text-left">
+        <h1 class="font-bold text-7xl">{{ article.title }}</h1>
+        <p class="text-2xl my-5" style="color: #4a5568">{{ article.body }}</p>
+        <div class="flex justify-between">
+          <span class="text-2xl" style="color: #4a5568">{{
+            article.created_at
+          }}</span>
+          <span
+            class="font-bold text-2xl cursor-pointer opacity-100 hover:opacity-70"
+            >Read more</span
+          >
+        </div>
+      </div>
+      <div class="w-1/2">
+        <img class="h-full" :src="article2.image" :alt="article.title" />
+      </div>
+    </div>
+  </template>
+</template>
+<script setup>
+import Img from "../../assets/images/content.png";
+import ComputerImage from "../../assets/images/computer.png";
+
+import { defineProps, ref } from "vue";
+
+const props = defineProps({
+  articleHeight: Number,
+  articleType: String,
+});
+
+const height = ref(props.articleHeight);
+
+class Article {
+  constructor(title, image, created_at, likes, body) {
+    this.title = title;
+    this.image = image;
+    this.created_at = created_at;
+    this.likes = height;
+    this.body = body;
+  }
+}
+
+let article = new Article(
+  "long established",
+  Img,
+  new Date().toISOString().split("T")[0],
+  Math.floor(Math.random()) + 1,
+  "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that...."
+);
+
+let article2 = new Article(
+  "long established",
+  ComputerImage,
+  new Date().toISOString().split("T")[0],
+  Math.floor(Math.random()) + 1,
+  "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that...."
+);
+</script>
